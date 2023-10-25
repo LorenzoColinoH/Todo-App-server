3f9d1247cbffba7ed14aa98899ba633c8aba3cdf
LorenzoColinoH <lorenzocolinoh@gmail.com>
2023-10-25 16:55:08 +0200

    New endpoints added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-//mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false);
 //mongoose.connect("mongodb://0.0.0.0:27017/base")
 mongoose.connect("mongodb+srv://lorenzocolinoh:hola123@clustertodo.le6wbvt.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -23,18 +23,34 @@ app.get("/todos/:userId", autenticado ,async (req,res)=> {
     res.json(todos)
 })
 
+app.get("/todos/title/:title", async (req, res)=> {
+    const {title} = req.params
+    const todosWithSameTitle = await Todo.find({title})
+    if (todosWithSameTitle){
+        res.status(200).json(todosWithSameTitle)
+    }
+    else {
+       res.status(404).send("No hay todos con ese título") 
+    }
+})
+
 app.post("/newTodo", async (req,res)=> {
     const {title, userId} = req.body
-    const newTodo = new Todo({title: title, userId: userId})
-    try{
-        await newTodo.save()
-        res.status(200).send("Creado")
-        return 
+    if (title && userId){
+        const newTodo = new Todo({title: title, userId: userId})
+        try{
+            await newTodo.save()
+            res.status(200).send("Creado")
+            return 
+        }
+        catch(e){
+            console.log(e)
+            res.status(400).send("Vuelve a intentarlo")
+            return
+        }
     }
-    catch(e){
-        console.log(e)
-        res.status(400).send("Vuelve a intentarlo")
-        return
+    else{
+        res.status(404).send("Usuario o título no disponibles en la petición")
     }
 })
 app.delete("/delete/:userId/:title", async (req,res)=> {
@@ -46,7 +62,8 @@ app.delete("/delete/:userId/:title", async (req,res)=> {
 app.put("/todo/:id", async (req,res)=> {
     const {title} = req.body
     const { id } = req.params
-    const todo = Todo.find({_id: id})
+    const todo = (await Todo.find({_id: id}))[0]
+    console.log(title, id, todo)
     if (todo && title){
         todo.title = title
         await todo.save()
